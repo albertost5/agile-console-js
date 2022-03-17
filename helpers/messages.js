41f8f9e33f6d7cec57e257155c1aa9a077e1fa48
albertost5 <sancheztorresalberto@gmail.com>
2022-03-17 15:21:42 +0100

    This is a new commit for what I originally planned to be amended

--- helpers/messages.js
+++ helpers/messages.js
@@ -1,56 +1,56 @@
-const colors = require('colors');
-const menuOptionsArr = require('./menuOptions');
-const readline = require('readline');
+// const colors = require('colors');
+// const menuOptionsArr = require('./menuOptions');
+// const readline = require('readline');
 
 
-const showMenu = () => {
+// const showMenu = () => {
 
-    return new Promise( ( resolve, reject ) => {
+//     return new Promise( ( resolve, reject ) => {
 
-        console.clear();
-        console.log('=================='.cyan);
-        console.log('Select one option: '.cyan);
-        console.log('================== \n'.cyan);
+//         console.clear();
+//         console.log('=================='.cyan);
+//         console.log('Select one option: '.cyan);
+//         console.log('================== \n'.cyan);
     
-        let index = 0;
-        menuOptionsArr.forEach((menuOpt, index) => {
-            console.log(`${ index }.`.cyan + ` ${ menuOpt }`);
-            index++;
-        });
+//         let index = 0;
+//         menuOptionsArr.forEach((menuOpt, index) => {
+//             console.log(`${ index }.`.cyan + ` ${ menuOpt }`);
+//             index++;
+//         });
     
-        const rl = readline.createInterface( process.stdin, process.stdout );
+//         const rl = readline.createInterface( process.stdin, process.stdout );
     
-        rl.question('Select one option: '.green, opt => {
-            rl.close();
+//         rl.question('Select one option: '.green, opt => {
+//             rl.close();
 
-            let optToInt = Number.parseInt(opt);
+//             let optToInt = Number.parseInt(opt);
 
-            if(( opt && optToInt >= 0 && optToInt < menuOptionsArr.length && Number.isInteger(optToInt) )) {
-                resolve(optToInt);
-            }else {
-                if( !Number.isInteger( optToInt ) ) {
-                    reject('The input should be a number.'.red);
-                }else {
-                    reject(`The number selected should be between ${ 0 }-${ menuOptionsArr.length - 1 }.`.red);
-                }
-            }
-        });
+//             if(( opt && optToInt >= 0 && optToInt < menuOptionsArr.length && Number.isInteger(optToInt) )) {
+//                 resolve(optToInt);
+//             }else {
+//                 if( !Number.isInteger( optToInt ) ) {
+//                     reject('The input should be a number.'.red);
+//                 }else {
+//                     reject(`The number selected should be between ${ 0 }-${ menuOptionsArr.length - 1 }.`.red);
+//                 }
+//             }
+//         });
 
-    });
-}
+//     });
+// }
 
-const pause = () => {
-    return new Promise( resolve => {
-        const rl = readline.createInterface( process.stdin, process.stdout );
+// const pause = () => {
+//     return new Promise( resolve => {
+//         const rl = readline.createInterface( process.stdin, process.stdout );
     
-        rl.question(`Press ${ 'ENTER'.green } to continue.`, () => {
-            rl.close();
-            resolve();
-        });
-    })
-}
+//         rl.question(`Press ${ 'ENTER'.green } to continue.`, () => {
+//             rl.close();
+//             resolve();
+//         });
+//     })
+// }
 
-module.exports = { 
-    showMenu,
-    pause
-};
+// module.exports = { 
+//     showMenu,
+//     pause
+// };
